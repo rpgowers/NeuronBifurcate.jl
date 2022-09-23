e8e91d8aa9f7028ac69b9a46042402e73b1e7527
Robert Gowers <robert.gowers@hu-berlin.de>
2022-09-23 20:29:44 +0200

    added fixed point finding for MLS model

--- src/model_functions.jl
+++ src/model_functions.jl
@@ -1,7 +1,40 @@
-function fun_test()
-  println("The function works")
-  0
-end
-export fun_test
-
 include("model_arguments.jl")
+
+using Roots
+
+function a∞(v,A,Δ)
+  return 1/( 1+exp( -(v-A)/Δ ) )
+end
+export a∞
+
+function ψa(v,ϕ,A,Δ)
+  return ϕ*cosh( (v-A)/(4*Δ) )
+end
+export ψa
+
+function ML_ncurrent((n,v),args)
+  @unpack An, Δn, ϕ = args
+  return (a∞(v,An,Δn)-n)*ψa(v,ϕ,An,Δn)
+end
+export ML_ncurrent
+
+function MLS_voltage((n,v),args)
+  @unpack C, EL, gL, Ef, gf, Am, Δm, Es, gs, Iext = args
+  return (gL*(EL-v)+gs*n*(Es-v)+gf*a∞(v,Am,Δm)*(Ef-v)+Iext)/C
+end
+export MLS_voltage
+
+function F(x, args::MLS_Param)
+  [ML_ncurrent(x,args), MLS_voltage(x,args)]
+end
+export F
+
+function I∞(v,args::Union{MLS_Param})
+  return MLS_voltage((a∞(v,args.An,args.Δn),v),args)*args.C
+end
+export I∞
+
+function vfps(args)
+  vfp = find_zeros(v->I∞(v,args), -120.0, 50.0)
+end
+export vfps
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,14 @@ using Test
 
 @testset "NeuronBifurcate.jl" begin
     # Write your tests here.
-    @test NeuronBifurcate.fun_test() == 0
+    args = MLS_Param()
+    v = -50.0
+    n = 0.1
+    @test NeuronBifurcate.a∞(v, args.An, args.Δn) > 0
+    @test NeuronBifurcate.ψa(v, args.ϕ, args.An, args.Δn) > 0
+    @test NeuronBifurcate.ML_ncurrent((n,v),args) > -Inf
+    @test NeuronBifurcate.MLS_voltage((n,v),args) > -Inf
+    @test length(NeuronBifurcate.F((n,v), args)) == 2
+    @test NeuronBifurcate.I∞(v,args) > -Inf
+    @test length(NeuronBifurcate.vfps(args)) == 3
 end
